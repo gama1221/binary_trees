90dcc4f24915cd4b26a9afa6b2d634b144217eb1
ase1234 <asefa.mulu54@gmail.com>
2023-02-28 21:00:04 +0300

    Adding project files

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -3,10 +3,12 @@
 /**
  * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of integers
  *
- * @root: double pointer to the root node of heap
- * Return: value stored in the root node
+ * @heap: pointer to the root node of the heap to convert
+ * @size: address to store the size of the array
+ * Return: array sorted
  */
-int heap_extract(heap_t **root)
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	return (root);
+	return (heap);
 }
+
